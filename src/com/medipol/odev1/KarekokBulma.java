d573a8ef8616b49adc9bec4384b6cd9ee050a548
sefaorucc <sefacan.oruc@std.medipol.edu.tr>
2021-11-04 22:56:17 +0300

    Bu odev yapildi.

--- src/com/medipol/odev1/KarekokBulma.java
+++ src/com/medipol/odev1/KarekokBulma.java
@@ -34,7 +34,7 @@ public class KarekokBulma {
                x2 = (-b - Math.sqrt(delta)) / (2 * a);
 
              System.out.println("x1=" + x1);
-              System.out.println("x2=" + x2);
+             System.out.println("x2=" + x2);
         }
     }
 
